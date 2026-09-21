509411c3874d60992d14ac68742187facca3b691
Armands Leinieks <armands.leinieks@gmail.com>
2026-09-21 21:05:03 +0300

    Support Laravel MCP 1.0

--- src/Http/Controllers/McpDocumentationController.php
+++ src/Http/Controllers/McpDocumentationController.php
@@ -4,14 +4,25 @@ namespace Sezy\LaravelMcpDocumentationGenerator\Http\Controllers;
 
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use LogicException;
 use Sezy\LaravelMcpDocumentationGenerator\Discovery\McpDocumentationRepository;
 
 class McpDocumentationController
 {
     public function __invoke(McpDocumentationRepository $repository, Factory $views): View
     {
-        return $views->make('mcp-documentation-generator::index', [
+        return $this->render($views, 'mcp-documentation-generator::index', [
             'servers' => $repository->servers(),
         ]);
+    }
+
+    /** @param array<string, mixed> $data */
+    private function render(Factory $views, string $view, array $data): View
+    {
+        if (! $views->exists($view)) {
+            throw new LogicException('The MCP documentation view is not registered.');
+        }
+
+        return $views->make($view, $data);
     }
 }
